1310339ea5b4ce249a13ea9cf42fed851c7a937d
chien-le window <chien.hcckt@gmail.com>
2023-06-07 23:27:42 +0700

    slide-1 fix

--- resources/views/pages/home-page/section1.blade.php
+++ resources/views/pages/home-page/section1.blade.php
@@ -11,23 +11,23 @@
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="{{asset('images/sec1/mom-son.jpg')}}" class="d-block w-100" alt="img-slide">
+                <img src="{{asset('images/sec1/mom-son.jpg')}}" class="d-block " alt="img-slide">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('images/sec1/mom-son.jpg')}}" class="d-block w-100" alt="img-slide">
+                <img src="{{asset('images/sec1/mom-son.jpg')}}" class="d-block " alt="img-slide">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('images/sec1/mom-son.jpg')}}" class="d-block w-100" alt="img-slide">
+                <img src="{{asset('images/sec1/mom-son.jpg')}}" class="d-block " alt="img-slide">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
                 data-bs-slide="prev">
-            <img src="{{asset('images/arrow-l.png')}}" alt="" style="">
+            <img src="{{asset('images/arrow-l.png')}}" alt="" style="" class="img-fluid">
             <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
                 data-bs-slide="next">
-            <img src="{{asset('images/arrow-r.png')}}" alt="" style="">
+            <img src="{{asset('images/arrow-r.png')}}" alt="" style="" class="img-fluid">
             <span class="visually-hidden">Next</span>
         </button>
     </div>
@@ -38,10 +38,23 @@
     }
     .carousel-control-prev {
         margin-left: -4%;
+        z-index: 9999;
     }
 
     .carousel-control-next {
         margin-right: -4%;
+        z-index: 9999;
+    }
+    @media only screen and (max-width: 800px) {
+        .carousel-control-prev {
+            margin-left: 5%;
+            scale: 70%;
+        }
+
+        .carousel-control-next {
+            margin-right: 5%;
+            scale: 70%;
+        }
     }
     .btn-outline-white {
         border-color: white;
