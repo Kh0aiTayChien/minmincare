9d8e2887ffd5b8eb891ffaceb95132ea9eea5b16
Vinh <getback1110@gmail.com>
2023-06-12 04:45:30 +0700

    mobile homepage

--- resources/views/page-layout/header.blade.php
+++ resources/views/page-layout/header.blade.php
@@ -33,7 +33,6 @@
             </li>
         </ul>
     </div>
-
     <div class="right-action">
         <div><img src="{{asset('images/sec1/cart.png')}}" alt=""></div>
         <br>
@@ -41,6 +40,7 @@
     </div>
 
 </div>
+
 <style>
     .right-action {
         margin-right: 2%;
@@ -53,6 +53,37 @@
         display: none;
     }
 </style>
+
+<div class="headermb fixed-top">
+    <div class="inline-container justify-content-center align-items-center">
+        <img src="{{asset('images/header/Group 903.svg')}}" class="logomb">
+        <img src="{{asset('images/header/Group 1299.png')}}" class="menu">
+    </div>
+    <nav>
+        <ul class="hide iconmb">
+            <li class="contact linetop">
+                <a href="#sec2" class="linktext aniUtil_dramatic ani_fadeIn"><p>VỀ BĐS CƯ TRÚ</p></a>
+            </li>
+            <li class="contact linemenu">
+                <a href="#sec3" class="linktext aniUtil_dramatic ani_fadeIn"><p>VỀ CHƯƠNG TRÌNH</p></a>
+            </li>
+            <li class="contact linemenu">
+                <a href="#sec5" class="linktext aniUtil_dramatic ani_fadeIn"><p>TỔNG QUAN BĐS HUNGARY</p></a>
+            </li>
+            <li class="contact linemenu">
+                <a href="#sec6" class="linktext aniUtil_dramatic ani_fadeIn"><p>LỢI ÍCH VÀ QUYỀN LỢI</p></a>
+            </li>
+            <li class="contact linemenu">
+                <a href="#sec9" class="linktext aniUtil_dramatic ani_fadeIn"><p>ĐIỀU KIỆN VÀ QUY TRÌNH</p></a>
+            </li>
+            <li class="contact linemenu">
+                <a href="#addInformationmb" class="linktext aniUtil_dramatic ani_fadeIn"><p>LIÊN HỆ</p></a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+
 <script>
     $(document).ready(function() {
         $('.list-product').hover(
@@ -63,5 +94,22 @@
                 $(this).find('.list-product-button').slideUp(300); // Ẩn từ từ khi di chuột ra
             }
         );
+    });
+    $(document).ready(function () {
+        const menu = $('.menu');
+        const hiddenList = $('.hide');
+        const navLinks = $('nav ul li a');
+        const navMenu = $('nav ul');
+
+        // Thêm trình nghe sự kiện click cho menu element
+        menu.on('click', function () {
+            // Toggle class "hide" trên phần tử ul
+            hiddenList.toggleClass('hide');
+        });
+
+        // Thêm trình nghe sự kiện click cho mỗi liên kết trong danh sách
+        navLinks.on('click', function () {
+            hiddenList.addClass('hide'); // Thêm class 'hide' để ẩn danh sách
+        });
     });
 </script>
--- resources/views/pages/home-page/sec4.blade.php
+++ resources/views/pages/home-page/sec4.blade.php
@@ -68,3 +68,73 @@
         </a>
     </div>
 </div>
+
+<div class="sec4mb justify-content-center align-items-center">
+    <p class="header1sec4mb">NGŨ CỐC DINH DƯỠNG CHO MẸ BẦU</p>
+    <p class="contentsec4mb">Cung cấp đầy đủ dưỡng chất cần thiết cho mẹ bầu và thai nhi như axit folic, chất xơ,
+        vitamin D, sắt, canxi,
+        omega-3, chất chống oxy hóa. Sản phẩm được dựa trên công thức CO-NUMAX đột phá được nghiên cứu độc quyền
+        nhằm
+        giúp tối đa hóa nhu cầu dinh dưỡng toàn diện dành riêng cho mẹ bầu.</p>
+    <img src="{{asset('images/sec4/Group 1343.png')}}" class="bannersec4mb">
+    <a href="#">
+        <svg class="button2mb" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+             width="283.2" height="90.24" viewBox="0 0 283.2 90.24">
+            <g id="Group_881" data-name="Group 881" transform="translate(-1005 -3794.557)">
+                <g id="Group_880" data-name="Group 880">
+                    <g id="Group_879" data-name="Group 879">
+                        <g id="Group_877" data-name="Group 877">
+                            <g id="Group_876" data-name="Group 876">
+                                <g id="Group_875" data-name="Group 875">
+                                    <path id="Path_1519" data-name="Path 1519"
+                                          d="M1238.005,3807.6a27,27,0,1,1,0,54H1045.239a27,27,0,0,1,0-54h192.766m0-3H1045.239a30,30,0,0,0-30,30h0a30,30,0,0,0,30,30h192.766a30,30,0,0,0,30-30h0a30,30,0,0,0-30-30Z"
+                                          fill="#fff"/>
+                                </g>
+                            </g>
+                        </g>
+                        <g id="Group_878" data-name="Group 878">
+                            <text class="testsec4" id="XEM_CHI_TIẾT" data-name="XEM CHI TIẾT"
+                                  transform="translate(1140.522 3842.765)" fill="#fff" font-size="22"
+                                  font-family="Mitr-Medium, Mitr" letter-spacing="0.1em">
+                                <tspan x="-77.622" y="0">XEM CHI TIẾT</tspan>
+                            </text>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </a>
+    <p class="header1sec4mb">NGŨ CỐC LỢI SỮA CHO MẸ SAU SINH</p>
+    <p class="contentsec4mb">Tăng cường khả năng sản xuất lượng sữa tự nhiên cho mẹ chỉ sau 1 tuần sử dụng. Ngoài bổ
+        sung dinh dưỡng, khoáng chất, vitamin mà còn giúp mẹ sau sinh hồi phục sức khỏe, tăng sức đề kháng, hệ miễn
+        dịch, ngăn ngừa lão hóa, nhanh lành vết thương nhờ vào việc ứng dụng công nghệ Nano Curcumin vào sản phẩm.</p>
+    <img src="{{asset('images/sec4/Group 1343.png')}}" class="bannersec4mb">
+    <a href="#">
+        <svg class="button2mb" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+             width="283.2" height="90.24" viewBox="0 0 283.2 90.24">
+            <g id="Group_881" data-name="Group 881" transform="translate(-1005 -3794.557)">
+                <g id="Group_880" data-name="Group 880">
+                    <g id="Group_879" data-name="Group 879">
+                        <g id="Group_877" data-name="Group 877">
+                            <g id="Group_876" data-name="Group 876">
+                                <g id="Group_875" data-name="Group 875">
+                                    <path id="Path_1519" data-name="Path 1519"
+                                          d="M1238.005,3807.6a27,27,0,1,1,0,54H1045.239a27,27,0,0,1,0-54h192.766m0-3H1045.239a30,30,0,0,0-30,30h0a30,30,0,0,0,30,30h192.766a30,30,0,0,0,30-30h0a30,30,0,0,0-30-30Z"
+                                          fill="#fff"/>
+                                </g>
+                            </g>
+                        </g>
+                        <g id="Group_878" data-name="Group 878">
+                            <text class="testsec4" id="XEM_CHI_TIẾT" data-name="XEM CHI TIẾT"
+                                  transform="translate(1140.522 3842.765)" fill="#fff" font-size="22"
+                                  font-family="Mitr-Medium, Mitr" letter-spacing="0.1em">
+                                <tspan x="-77.622" y="0">XEM CHI TIẾT</tspan>
+                            </text>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </a>
+</div>
+
