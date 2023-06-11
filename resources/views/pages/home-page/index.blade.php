efe58c530c808c7c4b1ce04deeccdfb801287510
Vinh <getback1110@gmail.com>
2023-06-12 04:52:14 +0700

    mobile homepage

--- resources/views/pages/home-page/index.blade.php
+++ resources/views/pages/home-page/index.blade.php
@@ -1,10 +1,10 @@
 @extends('page-layout.master')
 @section('section')
-{{--    @include('pages.home-page.section1')--}}
+    @include('pages.home-page.section1')
     @include('pages.home-page.sec2')
     @include('pages.home-page.sec3')
     @include('pages.home-page.sec4')
-{{--    @include('pages.home-page.section5')--}}
+    @include('pages.home-page.section5')
     @include('pages.home-page.sec6')
     @include('pages.home-page.sec10')
 @stop
