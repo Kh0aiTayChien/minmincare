41367c176d7765757bc2ac1d444bc756992d37b3
Vinh <getback1110@gmail.com>
2023-06-26 12:04:34 +0700

    responesive

--- resources/views/pages/gioi-thieu/sec5.blade.php
+++ resources/views/pages/gioi-thieu/sec5.blade.php
@@ -9,3 +9,22 @@
         </span></p>
     <a href="#"><img src="{{asset('images/gioi-thieu/sec5/Group 7693.svg')}}" class="btn-start"></a>
 </div>
+
+<div class="sec5mb-GT">
+    <p class="textsec5mb-GT">Do kinh tế khó khăn nên Ms Nga đi làm sớm hơn 6 tháng vì thế bị giảm sữa nhiều. Là một
+        người mẹ, Ms Nga nhận thức được rất rõ nỗi đau khi không đủ sữa cho con.</p>
+    <p class="textsec5mb-GT">Qua nghiên cứu từ sách vở và nguồn nước ngoài uy tín Ms Nga đã khám phá ra bột ngũ cốc từ
+        các loại hạt giúp tăng cường lượng sữa tự nhiên. Tuy nhiên, chất lượng sản phẩm trên thị trường thời điểm đó
+        chưa đáp ứng được nhu cầu.</p>
+    <p class="textsec5mb-GT">Nên Ms Nga đã tự mình học hỏi và chế biến ra loại bột ngũ cốc mong muốn. Khi sử dụng thì
+        đạt hiệu quả ngoài mong đợi, vì thế Ms Nga chia sẻ lại với mọi người xung quanh. Không những được đón nhận nhiệt
+        tình mà còn rất nhiều người đặt mua sản phẩm.</p>
+    <img src="{{asset('images/gioi-thieu/sec5/Group 1328.png')}}" class="bannersec5mb-GT">
+    <p class="textsec5mb-GT">Sau đó Ms Nga có cơ duyên hợp tác với đội ngũ chuyên gia tại viện dinh dưỡng quốc gia và
+        nghiên cứu ra
+        được công thức CO-NUMAX đột phá so với các sản phẩm khác thời điểm đó.</p>
+    <p class="textsec5mb-GT">Min Min Care không chỉ là một thương hiệu với sản phẩm ngũ cốc dinh dưỡng đơn thuần, mà còn
+        là niềm hy vọng, niềm tin vào sự phát triển và tiến bộ. Bởi chính trải nghiệm và mong muốn của Ms Nga, sản phẩm
+        luôn luôn được nâng cao nhằm mang đến bữa ăn dinh dưỡng toàn diện cho cả mẹ bầu và mẹ sau sinh.</p>
+    <a href="#"><img src="{{asset('images/gioi-thieu/sec5/Group 7693.svg')}}" class="btn-startmb-GT"></a>
+</div>
