6957381655aaf2722f457376edfb50eba06418d7
Vinh <getback1110@gmail.com>
2023-06-28 13:56:59 +0700

    fix loi 28/06 13:56

--- resources/views/pages/tin-tuc/sec2.blade.php
+++ resources/views/pages/tin-tuc/sec2.blade.php
@@ -1,12 +1,12 @@
 <div class="sec2-TT">
-    <div class="card flex-row newscontent col-xxl-9 col-sm-10 row-test">
-        <img class="card-img-left col-3" src="{{asset('images/tin-tuc/sec2/Asset 1.png')}}"/>
-        <div class="card-body col-9">
+    <div class="card flex-row newscontent col-xxl-9 col-sm-10 col-10 row-test">
+        <img class="card-img-left col-xxl-3 col-sm-3 col-10" src="{{asset('images/tin-tuc/sec2/Asset 1.png')}}"/>
+        <div class="card-body col-xxl-9 col-sm-9">
             <div class="datetime col-7">
-                <img src="{{asset('images/tin-tuc/sec2/Asset 3.svg')}}" class="col-3 imgdatetime">
+                <img src="{{asset('images/tin-tuc/sec2/Asset 3.svg')}}" class="imgdatetime">
                 <p class="textdatetime">09/06/2023</p>
             </div>
-            <a href="#" class="linknews col-9"><h4 class="card-title h5 h4-sm titlenews">Ngũ cốc dinh dưỡng Min Min Care đạt chứng nhận FDA</h4></a>
+            <a href="#" class="linknews"><h4 class="card-title h5 h4-sm titlenews col-12">Ngũ cốc dinh dưỡng Min Min Care đạt chứng nhận FDA</h4></a>
             <p class="card-text col-12 titletext">Min Min Care là thương hiệu tiên phong về ngũ cốc dinh dưỡng cao cấp dành riêng cho mẹ
                 bầu và mẹ sau sinh. Được nghiên cứu bởi đội ngũ chuyên gia viện dinh dưỡng quốc gia và sản xuất từ nguồn
                 nguyên liệu 100% tự nhiên với công thức đột phá, độc quyền sở hữu....</p>
