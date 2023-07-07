8e40f421210c0656a71676e1067f27cf306e5fb1
Vinh <getback1110@gmail.com>
2023-07-07 15:03:49 +0700

    mobile Ngamin

--- resources/views/pages/nga-min/sec2.blade.php
+++ resources/views/pages/nga-min/sec2.blade.php
@@ -17,18 +17,19 @@
                 bỏ. Nhưng Nga vẫn giữ vững niềm tin bước tiếp, dần dần từng bước một để đi qua khó khăn bằng những kinh
                 nghiệm đã tích lũy ngày trước.</p>
             <button type="button" class="imgbtn-NM" data-bs-toggle="modal"
-                    data-bs-target="#exampleModal-NM">
+                    data-bs-target="#exampleModalsec2-NM">
                 <img src="/images/nga-min/sec1/Asset 1.png" class="buttonvideo-NM col-xxl-12 col-md-12">
             </button>
-            <div class="modal fade" id="exampleModal-NM" tabindex="-1" aria-labelledby="exampleModalLabel"
+            <div class="modal fade" id="exampleModalsec2-NM" tabindex="-1" aria-labelledby="exampleModalLabel"
                  aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-fullscreen">
                     <div class="modal-content d-flex flex-row-reverse" style="background-color: rgba(0, 0, 0, 0.5);">
-                        <button id="btn-close-NM" type="button" class="btn-close" data-bs-dismiss="modal"
+                        <button id="btn-close-sec2-NM" type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         <div class="modal-body d-flex justify-content-center align-items-center">
                             <div>
-                                <iframe id="video-iframe-NM" width="1100" height="600" class="embed-responsive-item"
+                                <iframe id="video-iframe-sec2-NM" width="1100" height="600"
+                                        class="embed-responsive-item"
                                         src="https://www.youtube.com/embed/rExo9QFiew4"
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -41,7 +42,8 @@
         </div>
         <div class="col-xxl-4 col-md-5 containersec2-GT-test">
             <img src="{{asset('images/nga-min/sec2/Asset 2.png')}}" class="col-xxl-4 col-md-5 imgsec2-NM">
-            <p class="text2sec2-GT-test text-NM">Sau đó Nga có cơ duyên hợp tác và được nghiên cứu độc quyền cùng với đội ngũ
+            <p class="text2sec2-GT-test text-NM">Sau đó Nga có cơ duyên hợp tác và được nghiên cứu độc quyền cùng với
+                đội ngũ
                 chuyên gia hàng đầu tại viện dinh dưỡng quốc gia và các chuyên gia giàu kinh nghiệm, uy tín từ nước
                 ngoài để cho ra được công thức CO-NUMAX đột phá so với các sản phẩm khác thời điểm đó.
             </p>
@@ -52,3 +54,66 @@
         </div>
     </div>
 </div>
+
+<div class="sec2mb-NM">
+    <p class="contentsec2mb-GT">Do kinh tế khó khăn nên Nga đi làm sớm hơn 6 tháng vì thế bị giảm sữa nhiều. Con thì
+        không chịu uống sữa ngoài, nên mình rất lo cho con.</p>
+    <p class="contentsec2mb-GT">Qua nghiên cứu từ sách vở và nguồn nước ngoài uy tín Nga đã khám phá ra bột ngũ cốc từ
+        các loại hạt giúp tăng cường lượng sữa cho con. Tuy nhiên, chất lượng sản phẩm trên thị trường thời điểm đó chưa
+        đáp ứng được nhu cầu của Nga.</p>
+    <p class="contentsec2mb-GT">Nên Nga đã tự mình học hỏi và chế biến ra loại bột ngũ cốc mong muốn. Khi sử dụng thì
+        đạt hiệu quả ngoài mong đợi, vì thế Nga chia sẻ lại với mọi người xung quanh. Không những được đón nhận nhiệt
+        tình mà còn rất nhiều người đặt mua sản phẩm.</p>
+    <p class="contentsec2mb-GT">Thời gian đầu chưa có vốn nên không dễ dàng để vận hành hay sản xuất số lượng lớn nên
+        rất dễ từ bỏ. Nhưng Nga vẫn giữ vững niềm tin bước tiếp, dần dần từng bước một để đi qua khó khăn bằng những
+        kinh nghiệm đã tích lũy ngày trước.</p>
+    <div class="utubesec2mb-NM" style="border-radius: 10px; overflow: hidden; width: 86vw; height: 180px">
+        <iframe class="utub" id="youtube-iframe-sec2-NM" src="https://www.youtube.com/embed/aSM49MxLHoc" frameborder="0"
+                allowfullscreen></iframe>
+    </div><br>
+    <p class="contentsec2mb-GT">Sau đó Nga có cơ duyên hợp tác và được nghiên cứu độc quyền cùng với đội ngũ chuyên gia
+        hàng đầu tại viện dinh dưỡng quốc gia và các chuyên gia giàu kinh nghiệm, uy tín từ nước ngoài để cho ra được
+        công thức CO-NUMAX đột phá so với các sản phẩm khác thời điểm đó.</p>
+    <p class="contentsec2mb-GT">Tên gọi thương hiệu được lấy cảm hứng từ tên con trai yêu quý của Nga.</p>
+    <p class="contentsec2mb-GT">Min Min Care không chỉ là một sản phẩm ngũ cốc dinh dưỡng đơn thuần, mà còn là niềm hy
+        vọng, niềm tin vào sự phát triển và tiến bộ. Bởi chính trải nghiệm và mong muốn của Nga, sản phẩm luôn luôn được
+        nâng cao nhằm mang đến bữa ăn dinh dưỡng toàn diện cho cả mẹ bầu và mẹ sau sinh.</p>
+</div>
+
+<script>
+    var modal = document.getElementById('exampleModalsec2-NM');
+    var iframe = document.getElementById('video-iframe-sec2-NM');
+    var btnClose = document.getElementById('btn-close-sec2-NM');
+
+    // Bắt sự kiện click chuột với capturing
+    window.addEventListener('click', function NMsec2(event) {
+        // Kiểm tra nếu người dùng đã nhấp chuột bên ngoài iframe
+        if (!iframe.contains(event.target) && !btnClose.contains(event.target)) {
+            // Chuyển hướng sự kiện click chuột đến nút đóng modal
+            btnClose.click();
+
+            // Dừng video khi modal đóng
+            iframe.src = iframe.src;
+        }
+    }, true);
+
+    // Hàm tính toán và đặt chiều rộng theo tỷ lệ màn hình điện thoại
+    function setIframeWidthsec2NM() {
+        var screenWidth = $(window).width();
+        var iframe = $('#youtube-iframe-sec2-NM');
+        var aspectRatio = 16 / 9; // Tỷ lệ màn hình mong muốn (16:9)
+        var targetWidth = Math.floor(screenWidth * 0.9); // Giả sử sử dụng 80% chiều rộng của màn hình
+
+        // Tính toán chiều cao dựa trên tỷ lệ màn hình
+        var targetHeight = Math.floor(targetWidth / aspectRatio);
+
+        // Đặt chiều rộng và chiều cao mới cho iframe
+        iframe.css({
+            width: targetWidth + 'px',
+            height: targetHeight + 'px'
+        });
+    }
+
+    // Gọi hàm setIframeWidth khi trang web được tải và khi thay đổi kích thước màn hình
+    $(window).on('load resize', setIframeWidthsec2NM);
+</script>
--- resources/views/pages/nga-min/sec4.blade.php
+++ resources/views/pages/nga-min/sec4.blade.php
@@ -42,6 +42,50 @@
     </div>
 </div>
 
+<div class="sec4mb-NM">
+    <p class="header1sec4-NM">NƠI NHỮNG ƯỚC MUỐN</p>
+    <p class="header2sec4-NM">Được chắp cánh</p>
+    <div class="containersec4-NM">
+        <p class="textsec4-NM">Nga và Min Min Care luôn mong muốn mang đến những điều tốt nhất không chỉ cho khách hàng mà cả những người
+            đang và sẽ trở thành những đối tác, đại lý của Min Min Care. Bạn sẽ thấy chính sách đại lý Min Min Care đặc
+            biệt bởi:</p>
+        <div class="col-9 row-test slidesec4-NM">
+            <div class="col-2 slide1-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 3.png')}}" class="img1sec4-NM">
+                <p class="textslidesec4-NM">Dành cho người muốn kiếm thêm thu nhập, muốn làm tại nhà - Đặc biệt phù hợp với mẹ bỉm sữa</p>
+            </div>
+            <div class="col-2 slide2-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 4.png')}}" class="img2sec4-NM">
+                <p class="textslidesec4-NM">Số vốn ban đầu thấp chỉ từ 1 đến 5 triệu để nhập sản phẩm. Không phát sinh bất kỳ chi phí nào khác</p>
+            </div>
+            <div class="col-2 slide3-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 5.png')}}" class="img3sec4-NM">
+                <p class="textslidesec4-NM">Chính sách chiết khấu/ quyền lợi cực kỳ hấp dẫn</p>
+            </div>
+            <div class="col-2 slide4-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 6.png')}}" class="img4sec4-NM">
+                <p class="textslidesec4-NM">Không lo ôm hàng vì sản phẩm đã được cộng đồng các mẹ tin dùng</p>
+            </div>
+            <div class="col-2 slide1-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 3.png')}}" class="img1sec4-NM">
+                <p class="textslidesec4-NM">Được đào tạo kiến thức từ A-Z kể cả nếu bạn lần đầu tiên làm đại lý</p>
+            </div>
+            <div class="col-2 slide2-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 4.png')}}" class="img2sec4-NM">
+                <p class="textslidesec4-NM">Được hỗ trợ truyền thông/ quảng cáo sản phẩm từ Min Min Care</p>
+            </div>
+            <div class="col-2 slide3-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 5.png')}}" class="img3sec4-NM">
+                <p class="textslidesec4-NM">Được đảm bảo uy tín bởi chính Nga Min Min - sáng lập thương hiệu Min Min Care</p>
+            </div>
+            <div class="col-2 slide4-NM">
+                <img src="{{asset('images/nga-min/sec4/Asset 6.png')}}" class="img4sec4-NM">
+                <p class="textslidesec4-NM">Được tham gia các chương trình/ hoạt động ý nghĩa do Min Min Care tổ chức</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     $(document).ready(function sec4NM() {
         $('.slidesec4-NM').slick({
