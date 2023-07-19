b45841b2825e853c554cc2c3f809f3c5d787a00e
chien-le window <chien.hcckt@gmail.com>
2023-07-19 16:26:22 +0700

    3 4 5 10

--- resources/views/pages/advertise/index.blade.php
+++ resources/views/pages/advertise/index.blade.php
@@ -1,4 +1,8 @@
 @extends('page-layout.master')
 @section('section')
-
+    @include('pages.advertise.section3')
+    @include('pages.advertise.section4')
+    @include('pages.advertise.section5')
+    @include('pages.advertise.section10')
+    @include('pages.home-page.section9')
 @stop
