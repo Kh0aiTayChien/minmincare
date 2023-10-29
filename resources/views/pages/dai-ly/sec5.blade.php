0dbe51a785f159f5ae96070132bda500162bae8e
Vinh <getback1110@gmail.com>
2023-10-30 03:01:23 +0700

    add link utube

--- resources/views/pages/dai-ly/sec5.blade.php
+++ resources/views/pages/dai-ly/sec5.blade.php
@@ -14,7 +14,7 @@
                         <div class="modal-body d-flex justify-content-center align-items-center">
                             <div>
                                 <iframe id="video-iframe-DL" width="1100" height="600" class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/rExo9QFiew4"
+                                        src="https://www.youtube.com/embed/JTS5b2IwfDA?si=HRqHSHGUm8NrrYb-"
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         allowfullscreen></iframe>
@@ -25,19 +25,19 @@
             </div>
         </div>
         <div class="col-md-4">
-            <button type="button" class="imgbtn" data-bs-toggle="modal" data-bs-target="#exampleModal-DL">
+            <button type="button" class="imgbtn" data-bs-toggle="modal" data-bs-target="#exampleModal-DL-2">
                 <img src="/images/dai-ly/sec5/Asset 3.png" class="buttonvideo-DL-mid col-xl-12 col-md-12">
             </button>
-            <div class="modal fade" id="exampleModal-DL" tabindex="-1" aria-labelledby="exampleModalLabel"
+            <div class="modal fade" id="exampleModal-DL-2" tabindex="-1" aria-labelledby="exampleModalLabel"
                  aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-fullscreen">
                     <div class="modal-content d-flex flex-row-reverse" style="background-color: rgba(0, 0, 0, 0.5);">
-                        <button id="btn-close-DL" type="button" class="btn-close" data-bs-dismiss="modal"
+                        <button id="btn-close-DL-2" type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         <div class="modal-body d-flex justify-content-center align-items-center">
                             <div>
-                                <iframe id="video-iframe-DL" width="1100" height="600" class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/rExo9QFiew4"
+                                <iframe id="video-iframe-DL-2" width="1100" height="600" class="embed-responsive-item"
+                                        src="https://www.youtube.com/embed/uSEmZhpDqfU?si=j-4tk3XRULmZZvWg"
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         allowfullscreen></iframe>
@@ -48,19 +48,19 @@
             </div>
         </div>
         <div class="col-md-4">
-            <button type="button" class="imgbtn" data-bs-toggle="modal" data-bs-target="#exampleModal-DL">
+            <button type="button" class="imgbtn" data-bs-toggle="modal" data-bs-target="#exampleModal-DL-3">
                 <img src="/images/dai-ly/sec5/Asset 3.png" class="buttonvideo-DL-right col-xl-12 col-md-12">
             </button>
-            <div class="modal fade" id="exampleModal-DL" tabindex="-1" aria-labelledby="exampleModalLabel"
+            <div class="modal fade" id="exampleModal-DL-3" tabindex="-1" aria-labelledby="exampleModalLabel"
                  aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-fullscreen">
                     <div class="modal-content d-flex flex-row-reverse" style="background-color: rgba(0, 0, 0, 0.5);">
-                        <button id="btn-close-DL" type="button" class="btn-close" data-bs-dismiss="modal"
+                        <button id="btn-close-DL-3" type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         <div class="modal-body d-flex justify-content-center align-items-center">
                             <div>
-                                <iframe id="video-iframe-DL" width="1100" height="600" class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/rExo9QFiew4"
+                                <iframe id="video-iframe-DL-3" width="1100" height="600" class="embed-responsive-item"
+                                        src="https://www.youtube.com/embed/T0WrhaY-CWg?si=g4DSaae3o4M3J5Nk"
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         allowfullscreen></iframe>
@@ -77,17 +77,17 @@
     <p class="header-test-orange-mb">Khách hàng</p>
     <p class="header-test-green-mb">TIN TƯỞNG LỰA CHỌN</p>
     <div class="utubesec5-DL DL-1" style="border-radius: 10px; overflow: hidden; width: 80vw; height: 150px">
-        <iframe class="utub" id="youtube-iframe" src="https://www.youtube.com/embed/aSM49MxLHoc" frameborder="0"
+        <iframe class="utub" id="youtube-iframe" src="https://www.youtube.com/embed/JTS5b2IwfDA?si=HRqHSHGUm8NrrYb-" frameborder="0"
                 allowfullscreen></iframe>
         <br>
     </div>
     <div class="utubesec5-DL" style="border-radius: 10px; overflow: hidden; width: 80vw; height: 150px">
-        <iframe class="utub" id="youtube-iframe" src="https://www.youtube.com/embed/aSM49MxLHoc" frameborder="0"
+        <iframe class="utub" id="youtube-iframe-2" src="https://www.youtube.com/embed/uSEmZhpDqfU?si=j-4tk3XRULmZZvWg" frameborder="0"
                 allowfullscreen></iframe>
         <br>
     </div>
     <div class="utubesec5-DL" style="border-radius: 10px; overflow: hidden; width: 80vw; height: 150px">
-        <iframe class="utub" id="youtube-iframe" src="https://www.youtube.com/embed/aSM49MxLHoc" frameborder="0"
+        <iframe class="utub" id="youtube-iframe-3" src="https://www.youtube.com/embed/T0WrhaY-CWg?si=g4DSaae3o4M3J5Nk" frameborder="0"
                 allowfullscreen></iframe>
         <br>
     </div>
@@ -107,6 +107,38 @@
 
             // Dừng video khi modal đóng
             iframe.src = iframe.src;
+        }
+    }, true);
+
+    var modal2 = document.getElementById('exampleModal-DL-2');
+    var iframe2 = document.getElementById('video-iframe-DL-2');
+    var btnClose2 = document.getElementById('btn-close-DL-2');
+
+    // Bắt sự kiện click chuột với capturing
+    window.addEventListener('click', function DL(event) {
+        // Kiểm tra nếu người dùng đã nhấp chuột bên ngoài iframe
+        if (!iframe2.contains(event.target) && !btnClose2.contains(event.target)) {
+            // Chuyển hướng sự kiện click chuột đến nút đóng modal
+            btnClose2.click();
+
+            // Dừng video khi modal đóng
+            iframe2.src = iframe2.src;
+        }
+    }, true);
+
+    var modal3 = document.getElementById('exampleModal-DL-3');
+    var iframe3 = document.getElementById('video-iframe-DL-3');
+    var btnClose3 = document.getElementById('btn-close-DL-3');
+
+    // Bắt sự kiện click chuột với capturing
+    window.addEventListener('click', function DL(event) {
+        // Kiểm tra nếu người dùng đã nhấp chuột bên ngoài iframe
+        if (!iframe3.contains(event.target) && !btnClose3.contains(event.target)) {
+            // Chuyển hướng sự kiện click chuột đến nút đóng modal
+            btnClose3.click();
+
+            // Dừng video khi modal đóng
+            iframe3.src = iframe3.src;
         }
     }, true);
 </script>
--- resources/views/pages/dai-ly/sec6.blade.php
+++ resources/views/pages/dai-ly/sec6.blade.php
@@ -1,17 +1,17 @@
 <div class="sec6-DL">
     <object data="{{asset('images/dai-ly/sec6/Group 3172.png')}}" class="headersec6-DL"></object><br>
-    <button type="button" class="imgbtn" data-bs-toggle="modal" data-bs-target="#exampleModal-DL">
-        <img src="/images/dai-ly/sec5/Asset 3.png" class="buttonvideo-DL">
+    <button type="button" class="imgbtn" data-bs-toggle="modal" data-bs-target="#exampleModal-DL-4">
+        <img src="/images/dai-ly/sec5/Asset 3.png" class="buttonvideo-DL-4">
     </button>
-    <div class="modal fade" id="exampleModal-DL" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal-DL-4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-fullscreen">
             <div class="modal-content d-flex flex-row-reverse" style="background-color: rgba(0, 0, 0, 0.5);">
-                <button id="btn-close-DL" type="button" class="btn-close" data-bs-dismiss="modal"
+                <button id="btn-close-DL-4" type="button" class="btn-close" data-bs-dismiss="modal"
                         aria-label="Close"></button>
                 <div class="modal-body d-flex justify-content-center align-items-center">
                     <div>
-                        <iframe id="video-iframe-DL" width="1100" height="600" class="embed-responsive-item"
-                                src="https://www.youtube.com/embed/rExo9QFiew4"
+                        <iframe id="video-iframe-DL-4" width="1100" height="600" class="embed-responsive-item"
+                                src="https://www.youtube.com/embed/DQUm_Hk1fxk?si=3TZKhcMHhRpn1qm9"
                                 title="YouTube video player" frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowfullscreen></iframe>
@@ -26,8 +26,26 @@
     <p class="header-test-orange-mb">Đại gia đình</p>
     <p class="header-test-green-mb">MIN MIN CARE</p>
     <div class="utubesec6-DL" style="border-radius: 10px; overflow: hidden; width: 80vw; height: 150px">
-        <iframe class="utub" id="youtube-iframe" src="https://www.youtube.com/embed/aSM49MxLHoc" frameborder="0"
+        <iframe class="utub" id="youtube-iframe" src="https://www.youtube.com/embed/DQUm_Hk1fxk?si=3TZKhcMHhRpn1qm9" frameborder="0"
                 allowfullscreen></iframe>
         <br>
     </div>
 </div>
+
+<script>
+    var modal4 = document.getElementById('exampleModal-DL-4');
+    var iframe4 = document.getElementById('video-iframe-DL-4');
+    var btnClose4 = document.getElementById('btn-close-DL-4');
+
+    // Bắt sự kiện click chuột với capturing
+    window.addEventListener('click', function DL(event) {
+        // Kiểm tra nếu người dùng đã nhấp chuột bên ngoài iframe
+        if (!iframe4.contains(event.target) && !btnClose4.contains(event.target)) {
+            // Chuyển hướng sự kiện click chuột đến nút đóng modal
+            btnClose4.click();
+
+            // Dừng video khi modal đóng
+            iframe4.src = iframe4.src;
+        }
+    }, true);
+</script>
