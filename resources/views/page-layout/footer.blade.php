855c617c8307ba852fdb1dd983e22121d5359ad5
Vinh <getback1110@gmail.com>
2024-01-18 17:48:30 +0700

    add content footer

--- resources/views/page-layout/footer.blade.php
+++ resources/views/page-layout/footer.blade.php
@@ -1,76 +1,80 @@
 <div class="footerpc">
     <img src="{{asset('images/footer/Group 3841.png')}}" class="linefooter">
     <!-- Footer -->
-    <footer class="text-center text-lg-start text-muted">
-        <section class="">
-            <div class="container text-center text-md-start mt-5">
-                <!-- Grid row -->
-                <div class="row mt-3">
-                    <!-- Grid column -->
-                    <div class="col-md-3 contentLeftfooter">
-                        <img src="{{asset('images/footer/Group 930.png')}}" class="logofooter"><br>
-                        <a href="tel:0989985091" class="linkphone">
-                            <img src="{{asset('images/footer/Group 3886.png')}}" class="phoneicon">
-                        </a><br>
-                        <a href="#" class="linkemail">
-                            <img src="{{asset('images/footer/Group 3884.png')}}" class="emailicon">
-                        </a><br>
-                        <a href="#" class="linkadress">
-                            <img src="{{asset('images/footer/Group 3885.png')}}" class="adressicon">
-                        </a>
-
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 mx-auto mb-4 contentMidfooter">
-                        <!-- Links -->
-                        <h6 class="text-uppercase mb-4 intro">
-                            GIỚI THIỆU
-                        </h6>
-                        <p>
-                            <a href="https://ngaminmin.com/" class="intro1">Về Min Min Care</a>
-                        </p>
-                        <p class="intro2">
-                            <a href="https://ngaminmin.com/" class="intro1">Về ngũ cốc dinh dưỡng</a>
-                        </p>
-                        <h6 class="text-uppercase mb-4 intro">
-                            SẢN PHẨM
-                        </h6>
-                        <p>
-                            <a href="{{asset('san-pham')}}" class="intro1">Ngũ cốc cho mẹ bầu</a>
-                        </p>
-                        <p class="intro2">
-                            <a href="{{asset('san-pham')}}" class="intro1">Ngũ cốc cho mẹ sau sinh</a>
-                        </p>
-                        <p class="intro2">
-                            <a href="{{asset('san-pham')}}" class="intro1">Sữa hạt dinh duỡng </a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 contentRightFooter">
-                        <!-- Links -->
-                        <h6 class="text-uppercase mb-4 intro">KIẾN THỨC</h6>
-                        <p>
-                            <a href="{{asset('kien-thuc')}}" class="intro1">Chuyên mục sức khỏe</a>
-                        </p>
-                        <h6 class="text-uppercase mb-4 intro">TIN TỨC</h6>
-                        <p>
-                            <a href="{{asset('tin-tuc')}}" class="intro1">Tin tức Min Min Care</a>
-                        </p>
-                        <h6 class="text-uppercase mb-4 intro">LIÊN HỆ</h6>
-                        <p>
-                            <a href="{{asset('lien-he')}}" class="intro1">Đăng ký đại lý</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+    <footer class="text-center text-lg-start text-muted footer-test">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 contentLeftfooter">
+                    <img src="{{asset('images/footer/Group 930.png')}}" class="logofooter"><br>
+                    <p class="intro1" style="color: white; text-align: justify">CÔNG TY TNHH THƯƠNG MẠI VÀ DỊCH VỤ DINH
+                        DƯỠNG MIN MIN VIỆT NAM </p>
+                    <p class="intro1" style="color: white">Mã số thuế: 0109227288</p>
+                    <p class="intro1" style="color: white">Người đại diện: NGUYỄN ĐÌNH VĂN</p>
+                    <p class="intro1" style="color: white">Số điện thoại: 0989985091</p>
+                    <p class="intro1" style="color: white">Mail: info@minmincare.vn</p>
+                    <p class="intro1" style="color: white; text-align: justify">Địa chỉ: Tuyết Nghĩa, Quốc Oai, thành
+                        phố Hà Nội</p>
                 </div>
-                <!-- Grid row -->
+                <div class="col-md-3 mx-auto mb-4 contentMidfooter">
+                    <!-- Links -->
+                    <h6 class="text-uppercase mb-4 intro">
+                        GIỚI THIỆU
+                    </h6>
+                    <p>
+                        <a href="https://ngaminmin.com/" class="intro1">Về Min Min Care</a>
+                    </p>
+                    <p class="intro2">
+                        <a href="https://ngaminmin.com/" class="intro1">Về ngũ cốc dinh dưỡng</a>
+                    </p>
+                    <h6 class="text-uppercase mb-4 intro">
+                        SẢN PHẨM
+                    </h6>
+                    <p>
+                        <a href="{{asset('san-pham')}}" class="intro1">Ngũ cốc cho mẹ bầu</a>
+                    </p>
+                    <p class="intro2">
+                        <a href="{{asset('san-pham')}}" class="intro1">Ngũ cốc cho mẹ sau sinh</a>
+                    </p>
+                    <p class="intro2">
+                        <a href="{{asset('san-pham')}}" class="intro1">Sữa hạt dinh duỡng </a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-3 mx-auto mb-md-0 mb-4 contentRightFooter">
+                    <!-- Links -->
+                    <h6 class="text-uppercase mb-4 intro">KIẾN THỨC</h6>
+                    <p>
+                        <a href="{{asset('kien-thuc')}}" class="intro1">Chuyên mục sức khỏe</a>
+                    </p>
+                    <h6 class="text-uppercase mb-4 intro">TIN TỨC</h6>
+                    <p>
+                        <a href="{{asset('tin-tuc')}}" class="intro1">Tin tức Min Min Care</a>
+                    </p>
+                    <h6 class="text-uppercase mb-4 intro">LIÊN HỆ</h6>
+                    <p>
+                        <a href="{{asset('lien-he')}}" class="intro1">Đăng ký đại lý</a>
+                    </p>
+                </div>
+                <div class="col-md-3 mx-auto mb-md-0 mb-4">
+                    <h6 class="intro text-uppercase" style="color: white">Chính sách</h6> <br>
+                    <a class="intro1" style="color: white" href="{{asset('download/chinh-sach-bao-mat.rar')}}"
+                       download="chinh-sach-bao-mat.rar">Chính sách bảo mật thông tin</a> <br><br>
+                    <a class="intro1" style="color: white" href="{{asset('download/chinh-sach-thanh-toan.rar')}}"
+                       download="chinh-sach-bao-thanh-toan.rar">Chính sách thanh toán</a> <br><br>
+                    <a class="intro1" style="color: white" href="{{asset('download/chinh-sach-van-chuyen.rar')}}"
+                       download="chinh-sach-van-chuyen.rar">Chính sách vận chuyển</a> <br><br>
+                    <a class="intro1" style="color: white" href="{{asset('download/Chinh-sach-hoan-tien-doi-tra.rar')}}"
+                       download="Chinh-sach-hoan-tien-doi-tra.rar">Chính sách hoàn tiền, đổi trả</a>
+                </div>
+                <!-- Grid column -->
             </div>
-        </section>
+            <!-- Grid row -->
+        </div>
 
     </footer>
     <!-- Footer -->
@@ -79,12 +83,14 @@
 <div class="footermb">
     <img src="{{asset('images/footer/Group 4239.png')}}" class="linefootermb" id="Informationmb">
     <img src="{{asset('images/footer/Group 930.png')}}" class="logofootermb">
-    <div class="linkcontactmb">
-        <a href="tel:0989985091" class="linkphonemb"><img src="{{asset('images/footer/Group 5083.png')}}"
-                                                          class="phonelogomb"></a>
-        <a href="#" class="linkmailmb"><img src="{{asset('images/footer/Group 5084.png')}}" class="maillogomb"></a>
+    <div class="" style="margin-left: 5%; text-align: justify; width: 90%">
+        <p class="footermb-product">CÔNG TY TNHH THƯƠNG MẠI VÀ DỊCH VỤ DINH DƯỠNG MIN MIN VIỆT NAM</p>
+        <p class="footermb-product">Mã số thuế: 0109227288</p>
+        <p class="footermb-product">Người đại diện: NGUYỄN ĐÌNH VĂN</p>
+        <p class="footermb-product">Số điện thoại: 0989985091</p>
+        <p class="footermb-product">Mail: info@minmincare.vn</p>
+        <p class="footermb-product">Địa chỉ: Tuyết Nghĩa, Quốc Oai, thành phố Hà Nội</p>
     </div>
-    <a href="#" class="linkaddressmb"><img src="{{asset('images/footer/Group 5082.png')}}" class="addresslogomb"></a>
     <div class="contentfooter">
         <div class="leftfootermb">
             <svg class="footerleft" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -163,14 +169,15 @@
                         </g>
                     </a>
                     <a href="{{asset('san-pham')}}" class="footerlink">
-                    <g transform="matrix(1, 0, 0, 1, 93.44, 5096.67)" filter="url(#Ngũ_cốc_cho_mẹ_sau_sinh)">
-                        <text id="Ngũ_cốc_cho_mẹ_sau_sinh-2" data-name="Ngũ cốc cho mẹ sau sinh"
-                              transform="translate(4.85 448.97)" fill="#fff" font-size="35"
-                              font-family="Mulish-Medium, Mulish"
-                              font-weight="500" letter-spacing="0.05em">
-                            <tspan x="0" y="35">Ngũ cốc cho mẹ sau sinh</tspan>
-                        </text>
-                    </g></a>
+                        <g transform="matrix(1, 0, 0, 1, 93.44, 5096.67)" filter="url(#Ngũ_cốc_cho_mẹ_sau_sinh)">
+                            <text id="Ngũ_cốc_cho_mẹ_sau_sinh-2" data-name="Ngũ cốc cho mẹ sau sinh"
+                                  transform="translate(4.85 448.97)" fill="#fff" font-size="35"
+                                  font-family="Mulish-Medium, Mulish"
+                                  font-weight="500" letter-spacing="0.05em">
+                                <tspan x="0" y="35">Ngũ cốc cho mẹ sau sinh</tspan>
+                            </text>
+                        </g>
+                    </a>
                     <a href="https://ngaminmin.com/" class="footerlink">
                         <g transform="matrix(1, 0, 0, 1, 93.44, 5096.67)" filter="url(#Về_Min_Min_Care)">
                             <text id="Về_Min_Min_Care-2" data-name="Về Min Min Care" transform="translate(4.85 96.04)"
@@ -298,6 +305,30 @@
                     </g>
                 </g>
             </svg>
+        </div>
+        {{--        <div class="leftfootermb col-5">--}}
+        {{--            <p class="">giới thiệu</p>--}}
+        {{--            <a href="" class="">Về Min Min Care</a>--}}
+        {{--            <a href="" class="">Về ngũ cốc dinh dưỡng</a>--}}
+        {{--            <p class="">sản phẩm</p>--}}
+        {{--            --}}
+        {{--        </div>--}}
+    </div>
+    <div class="" style="margin-left: 5%; text-align: justify; width: 90%">
+        <p class="text-uppercase"
+           style="font-family: Mitr-Medium, sans-serif; font-size: 13px; filter: drop-shadow(4px 3px 3px rgba(0, 0, 0, 0.3)); color: white; margin-left: 2%;">
+            chính sách
+        </p>
+        <div class="policy">
+            <a class="policy2" style="color: white" href="{{asset('download/chinh-sach-bao-mat.rar')}}"
+               download="chinh-sach-bao-mat.rar"><p class="footermb-product"> Chính sách bảo mật thông tin</p></a><br>
+            <a class="policy2" style="color: white" href="{{asset('download/chinh-sach-thanh-toan.rar')}}"
+               download="chinh-sach-bao-thanh-toan.rar"><p class="footermb-product policy1"> Chính sách thanh toán</p></a> <br>
+            <a class="policy2" style="color: white" href="{{asset('download/chinh-sach-van-chuyen.rar')}}"
+               download="chinh-sach-van-chuyen.rar"><p class="footermb-product policy1"> Chính sách vận chuyển</p></a> <br>
+            <a class="policy2" style="color: white"
+               href="{{asset('download/Chinh-sach-hoan-tien-doi-tra.rar')}}"
+               download="Chinh-sach-hoan-tien-doi-tra.rar"><p class="footermb-product policy1"> Chính sách hoàn tiền, đổi trả</p></a>
         </div>
     </div>
 </div>
