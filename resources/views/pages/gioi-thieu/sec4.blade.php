271ae8784ddead5e15ae1349f740b0acd2a7a6f8
chien.le <chien.hcckt@gmail.com>
2025-03-10 03:05:51 +0700

    fix text

--- resources/views/pages/gioi-thieu/sec4.blade.php
+++ resources/views/pages/gioi-thieu/sec4.blade.php
@@ -1,16 +1,22 @@
 <div class="sec4mb-GT">
     <p class="header-test-green-mb-1">CHUẨN MỰC CAO CẤP</p>
-    <p class="header-test-orange-mb-1">về dinh dưỡng dành riêng cho</p>
+    <p class="header-test-orange-mb-1">và an toàn cho sức khỏe </p>
+    <p class="header-test-orange-mb-1">người tiêu dùng</p>
     <p class="header-test-green-mb-1">MẸ BẦU VÀ MẸ SAU SINH</p>
-    <p class="contentsec2mb-GT contentsec4mb-GT ">Min Min Care được lấy cảm hứng tên từ con trai của nhà sáng lập thương hiệu Ms Nga.
-        Không chỉ thể hiện tình yêu thương của một người mẹ đối với đứa con, mà còn là để luôn gợi nhắc sứ mệnh của
-        thương hiệu mang đến giải pháp dinh dưỡng, sức khỏe toàn diện cho mẹ, bé và cả gia đình Việt.</p>
+{{--    <p class="contentsec2mb-GT contentsec4mb-GT ">Min Min Care được lấy cảm hứng tên từ con trai của nhà sáng lập thương hiệu Ms Nga.--}}
+{{--        Không chỉ thể hiện tình yêu thương của một người mẹ đối với đứa con, mà còn là để luôn gợi nhắc sứ mệnh của--}}
+{{--        thương hiệu mang đến giải pháp dinh dưỡng, sức khỏe toàn diện cho mẹ, bé và cả gia đình Việt.</p>--}}
+{{--    <img src="{{asset('images/gioi-thieu/sec2/Group 1301.png')}}" class="imgsec4mb-GT">--}}
+{{--    <p class="contentsec2mb-GT">Hầu hết các bà mẹ đều nhất trí rằng làm mẹ là trải nghiệm hạnh phúc nhất trong cuộc--}}
+{{--        đời họ. Không ai có thể thay thế vai trò của người phụ nữ, bởi họ là một nửa thế giới này.</p>--}}
+{{--    <p class="contentsec2mb-GT">Để chăm sóc một gia đình khỏe mạnh và hạnh phúc, người mẹ cũng phải là một người--}}
+{{--        khoẻ mạnh và hạnh phúc. Và Min Min Care cũng hướng tới điều đó dưới vai trò một người đồng hành cùng người--}}
+{{--        mẹ để họ làm tròn thiên chức của mình.</p>--}}
+    <p class="contentsec2mb-GT contentsec4mb-GT ">TẦM NHÌN: Min Min Care trở thành biểu tượng niềm tin hàng đầu với hệ sinh thái
+        toàn diện về chăm sóc sức khỏe, sắc đẹp và ngành hàng tiêu dùng.</p>
     <img src="{{asset('images/gioi-thieu/sec2/Group 1301.png')}}" class="imgsec4mb-GT">
-    <p class="contentsec2mb-GT">Hầu hết các bà mẹ đều nhất trí rằng làm mẹ là trải nghiệm hạnh phúc nhất trong cuộc
-        đời họ. Không ai có thể thay thế vai trò của người phụ nữ, bởi họ là một nửa thế giới này.</p>
-    <p class="contentsec2mb-GT">Để chăm sóc một gia đình khỏe mạnh và hạnh phúc, người mẹ cũng phải là một người
-        khoẻ mạnh và hạnh phúc. Và Min Min Care cũng hướng tới điều đó dưới vai trò một người đồng hành cùng người
-        mẹ để họ làm tròn thiên chức của mình.</p>
+    <p class="contentsec2mb-GT">SỨ MỆNH: Min Min Care mang đến giải pháp từ thiên nhiên cao cấp, toàn diện,
+        chuyên biệt số 1 được nghiên cứu bởi đội ngũ chuyên gia hàng đầu trong nước và quốc tế.</p>
     <div class="headersec5mb-GT">
         <p class="header-test-orange-mb">Khởi nguồn</p>
         <p class="header-test-green-mb">THƯƠNG HIỆU</p>
@@ -21,16 +27,20 @@
 <div class="sec4-GT-test row-test">
     <img src="{{asset('images/gioi-thieu/sec2/Group 1301.png')}}" class=" imgsec4-GT-test col-md-5 col-xxl-4">
     <div class="col-md-5 col-xxl-4 containersec4-GT-test">
-        <object data="{{asset('images/gioi-thieu/sec4/Group 5 (1).png')}}" class="headersec4-GT-test"></object>
+        <object data="{{asset('images/gioi-thieu/sec4/fix.png')}}" class="headersec4-GT-test"></object>
         <br>
-        <p class="textsec4-GT-test">Min Min Care được lấy cảm hứng tên từ con trai của nhà sáng lập thương hiệu Ms Nga.
-            Không chỉ thể hiện tình yêu thương của một người mẹ đối với đứa con, mà còn là để luôn gợi nhắc sứ mệnh của
-            thương hiệu mang đến giải pháp dinh dưỡng, sức khỏe toàn diện cho mẹ, bé và cả gia đình Việt.</p>
-        <p class="textsec4-GT-test">Hầu hết các bà mẹ đều nhất trí rằng làm mẹ là trải nghiệm hạnh phúc nhất trong cuộc
-            đời họ. Không ai có thể thay thế vai trò của người phụ nữ, bởi họ là một nửa thế giới này.</p>
-        <p class="textsec4-GT-test">Để chăm sóc một gia đình khỏe mạnh và hạnh phúc, người mẹ cũng phải là một người
-            khoẻ mạnh và hạnh phúc. Và Min Min Care cũng hướng tới điều đó dưới vai trò một người đồng hành cùng người
-            mẹ để họ làm tròn thiên chức của mình.</p>
+        <p class="textsec4-GT-test">TẦM NHÌN: Min Min Care trở thành biểu tượng niềm tin hàng đầu với hệ sinh thái
+            toàn diện về chăm sóc sức khỏe, sắc đẹp và ngành hàng tiêu dùng.</p>
+        <p class="textsec4-GT-test">SỨ MỆNH: Min Min Care mang đến giải pháp từ thiên nhiên cao cấp, toàn diện,
+            chuyên biệt số 1 được nghiên cứu bởi đội ngũ chuyên gia hàng đầu trong nước và quốc tế.</p>
+{{--        <p class="textsec4-GT-test">Min Min Care được lấy cảm hứng tên từ con trai của nhà sáng lập thương hiệu Ms Nga.--}}
+{{--            Không chỉ thể hiện tình yêu thương của một người mẹ đối với đứa con, mà còn là để luôn gợi nhắc sứ mệnh của--}}
+{{--            thương hiệu mang đến giải pháp dinh dưỡng, sức khỏe toàn diện cho mẹ, bé và cả gia đình Việt.</p>--}}
+{{--        <p class="textsec4-GT-test">Hầu hết các bà mẹ đều nhất trí rằng làm mẹ là trải nghiệm hạnh phúc nhất trong cuộc--}}
+{{--            đời họ. Không ai có thể thay thế vai trò của người phụ nữ, bởi họ là một nửa thế giới này.</p>--}}
+{{--        <p class="textsec4-GT-test">Để chăm sóc một gia đình khỏe mạnh và hạnh phúc, người mẹ cũng phải là một người--}}
+{{--            khoẻ mạnh và hạnh phúc. Và Min Min Care cũng hướng tới điều đó dưới vai trò một người đồng hành cùng người--}}
+{{--            mẹ để họ làm tròn thiên chức của mình.</p>--}}
     </div>
     <div class="col-md-5 col-xxl-4 headersec5-GT">
         <object data="{{asset('images/gioi-thieu/sec5/Group 19.png')}}"
